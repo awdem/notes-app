ffe8c6648ff2f0e3d36956dca77763e6fbaab1cf
awdem <wdemontmollin@gmail.com>
2023-05-31 10:26:24 +0100

    modified displayNotes to add note to the main container div element

--- notesView.js
+++ notesView.js
@@ -10,7 +10,7 @@ class NotesView {
       const newDiv = document.createElement('div');
       newDiv.textContent = note;
       newDiv.className = 'note';
-      document.body.append(newDiv);
+      document.querySelector('#main-container').append(newDiv);
     });
   }
 }
